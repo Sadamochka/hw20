48374878b90c9d31b2d199481c4eea7bdbe29c5a
Your Name <you@example.com>
2023-08-27 21:59:25 +0300

    added hw20

---
+++ src/LoginForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TextField, Button } from '@mui/material';
+import Image from './img/block.png';
+import './App.css';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRememberMeChange = (event) => {
+    setRememberMe(event.target.checked);
+  };
+
+  const isEmailValid = /^[\w-]+@\w+\.\w{2,}$/.test(email);
+  const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+
+  return (
+    <div className='imputBlok'>
+      <h2>Login</h2>
+      <div className='sign'><img className="pfoto" src={Image} alt="" />
+      <h3>Sign in</h3></div>
+      
+      <TextField
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange}
+        error={!isEmailValid && email !== ''}
+        helperText={!isEmailValid && email !== '' && 'Invalid email'}
+      />
+      <br />
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        error={!isPasswordValid && password !== ''}
+        helperText={!isPasswordValid && password !== '' && 'Invalid password'}
+      />
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          checked={rememberMe}
+          onChange={handleRememberMeChange}
+        />
+        Remember me
+      </label>
+      <br />
+      <Button className='buttons'
+        variant="contained"
+        disabled={!isEmailValid || !isPasswordValid}
+      >
+        Login
+      </Button>
+      <br />
+      <Link to="/register">Register</Link>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/RegistrationForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TextField, Button } from '@mui/material';
+import Image from './img/block.png';
+import './App.css';
+
+const RegistrationForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const isNameValid = /^[A-Za-z]{3,}$/.test(firstName) && /^[A-Za-z]{3,}$/.test(lastName);
+  const isEmailValid = /^[\w-]+@\w+\.\w{2,}$/.test(email);
+  const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+
+  return (
+    <div className='imputBlok'>
+      <h2>Registration</h2>
+      <div className='sign'><img className="pfoto" src={Image} alt="" />
+      <h3>Sign up</h3></div>
+      
+      <TextField
+        label="First Name"
+        variant="outlined"
+        value={firstName}
+        onChange={handleFirstNameChange}
+        error={!isNameValid && firstName !== ''}
+        helperText={!isNameValid && firstName !== '' && 'Invalid name'}
+      />
+      <br />
+      <TextField
+        label="Last Name"
+        variant="outlined"
+        value={lastName}
+        onChange={handleLastNameChange}
+        error={!isNameValid && lastName !== ''}
+        helperText={!isNameValid && lastName !== '' && 'Invalid name'}
+      />
+      <br />
+      <TextField
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange}
+        error={!isEmailValid && email !== ''}
+        helperText={!isEmailValid && email !== '' && 'Invalid email'}
+      />
+      <br />
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        error={!isPasswordValid && password !== ''}
+        helperText={!isPasswordValid && password !== '' && 'Invalid password'}
+      />
+      <br />
+      <Button className='buttons'
+        variant="contained"
+        disabled={!isNameValid || !isEmailValid || !isPasswordValid}
+      >
+        Register
+      </Button>
+      <br />
+      <Link to="/login">Login</Link>
+    </div>
+  );
+};
+
+export default RegistrationForm;
